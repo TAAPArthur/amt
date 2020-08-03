cd232dc11010855e60368da73f1adbe2aa624603
Arthur Williams <taaparthur@gmail.com>
2020-08-02 21:00:22 -0700

    logging fixup

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from amt.args import parse_args
 
 
 if __name__ == "__main__":
-    logging.basicConfig(format='[%(filename)s:%(lineno)s - %(funcName)20s()]%(levelname)s:%(message)s', level=logging.INFO)
+    logging.basicConfig(format='[%(filename)s:%(lineno)s]%(levelname)s:%(message)s', level=logging.INFO)
     parse_args()
