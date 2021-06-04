2784e7fe054e34efda63ca63b10a6bba06541d0d
Arthur Williams <taaparthur@gmail.com>
2021-06-03 23:15:06 -0700

    When setting credentials via env allow for using an upper or lower case server id

--- amt/settings.py
+++ amt/settings.py
@@ -154,8 +154,10 @@ class Settings:
 
     def get_credentials(self, server_id: str) -> (str, str):
         """Returns the saved username, password"""
-        if self.env_override_prefix and os.getenv(self.env_override_prefix + server_id):
-            return os.getenv(self.env_override_prefix + server_id).split(self.credential_separator)
+        if self.env_override_prefix:
+            var = os.getenv(self.env_override_prefix + server_id) or os.getenv(self.env_override_prefix + server_id.upper())
+            if var:
+                return var.split(self.credential_separator)
         if self.password_manager_enabled and self.password_load_cmd:
             try:
                 logging.debug("Loading credentials for %s `%s`", server_id, self.password_load_cmd.format(server_id))
