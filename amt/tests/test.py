dc54deed96d008538f8800875d0ebb52ef99f408
Arthur Williams <taaparthur@gmail.com>
2021-07-22 01:46:59 -0700

    Clear cache every test

--- amt/tests/test.py
+++ amt/tests/test.py
@@ -39,10 +39,7 @@ import_sub_classes(servers, CustomServer, LOCAL_SERVERS)
 
 class TestApplication(Application):
     def __init__(self, real=False, local=False):
-        # Save cache in local directory
-        os.environ["XDG_CACHE_HOME"] = "./.cache"
         settings = Settings(home=TEST_HOME)
-        del os.environ["XDG_CACHE_HOME"]
         if os.path.exists(settings.get_cookie_file()):
             os.remove(settings.get_cookie_file())
         settings.env_override_prefix = None
