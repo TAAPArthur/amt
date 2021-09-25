b3e60252d3eb33a1082b6c3e1285aa3fc890bf84
Arthur Williams <taaparthur@gmail.com>
2021-09-24 17:30:27 -0700

    settings: Flip default behavior of force_page_parity_end
    Change the default behavior of force_page_parity_end to prepend the
    dummy page instead of appending it.

--- amt/settings.py
+++ amt/settings.py
@@ -71,7 +71,7 @@ class Settings:
     chapter_title_format = "{media_name}: #{chapter_number} {chapter_title}"
     disable_ssl_verification = False
     force_page_parity = 0  # When downloading MANGA, if not equal to the number of pages % 2, add a dummy page
-    force_page_parity_end = True  # Add dummy page to after (default) or before real pages
+    force_page_parity_end = False  # Add dummy page before (default) or after real pages
     keep_unavailable = False
     merge_ts_files = True
     post_process_cmd = None
