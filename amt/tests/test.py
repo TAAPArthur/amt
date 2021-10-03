TAAPArthur/amt

commit edfbffd486d5031119da66c055a9d1abbbb24881

clean: remove downloaded files when chapter_info has been removed

amt/media_reader.py
@@ -579,9 +579,9 @@ class MediaReader:
             self.bundles.clear()
             shutil.rmtree(self.settings.bundle_dir)
             os.mkdir(self.settings.bundle_dir)
-        for dir in os.listdir(self.settings.media_dir):
-            server = self.get_server(dir)
-            server_path = os.path.join(self.settings.media_dir, dir)
+        for server_dir in os.listdir(self.settings.media_dir):
+            server = self.get_server(server_dir)
+            server_path = os.path.join(self.settings.media_dir, server_dir)
             if server:
                 if include_external or not server.external:
                     for media_dir in os.listdir(server_path):
@@ -589,13 +589,21 @@ class MediaReader:
                         if media_path not in media_dirs:
                             logging.info("Removing %s because it has been removed", media_path)
                             shutil.rmtree(media_path)
-                        elif remove_read:
-                            media_data = media_dirs[media_path]
+                            continue
+                        media_data = media_dirs[media_path]
+                        if remove_read:
                             for chapter_data in media_data.get_sorted_chapters():
                                 chapter_path = self.settings.get_chapter_dir(media_data, chapter_data, skip_create=True)
                                 if chapter_data["read"] and os.path.exists(chapter_path):
                                     logging.info("Removing %s because it has been read", chapter_path)
                                     shutil.rmtree(chapter_path)
+
+                        chapter_dirs = {os.path.basename(self.settings.get_chapter_dir(media_data, chapter_data, skip_create=True)): chapter_data for chapter_data in media_data.get_sorted_chapters()}
+                        for chapter_dir in os.listdir(media_path):
+                            chapter_path = os.path.join(media_path, chapter_dir)
+                            if os.path.isdir(chapter_path) and chapter_path not in chapter_dirs:
+                                logging.info("Removing %s because chapter info has been removed", chapter_path)
+                                shutil.rmtree(chapter_path)
 
             elif remove_disabled_servers:
                 logging.info("Removing %s because it is not enabled", server_path)

amt/tests/test.py
@@ -1223,6 +1223,16 @@ class ArgsTest(MinimalUnitTestClass):
         parse_args(media_reader=self.media_reader, args=["clean", "--remove-read"])
         self.verify_no_chapters_downloaded()
 
+    def test_clean_read_already_removed(self):
+        media_data = self.add_test_media(self.test_server, limit=1)[0]
+        self.media_reader.download_unread_chapters()
+        self.media_reader.mark_read()
+        self.media_reader.save()
+        media_data["chapters"].clear()
+        self.media_reader.save()
+        parse_args(media_reader=self.media_reader, args=["clean"])
+        self.assertEqual(1, len(os.listdir(self.settings.get_media_dir(media_data))))
+
     def test_clean_servers(self):
         self.add_test_media(self.test_server)
         self.media_reader.download_unread_chapters()

commit 02b536fa9ea28dff44c71364459f89f592f5f0b2

Fixed bug causing all chapter files to be deleted on clean
MediaReader::clean would incorrectly think every chapter had already been
removed from our metadata and would proceed to remove it from disk to
match.

Caused by edfbffd486d5031119da66c055a9d1abbbb24881

amt/media_reader.py
@@ -573,10 +573,10 @@ class MediaReader:
                                     logging.info("Removing %s because it has been read", chapter_path)
                                     shutil.rmtree(chapter_path)
 
-                        chapter_dirs = {os.path.basename(self.settings.get_chapter_dir(media_data, chapter_data, skip_create=True)): chapter_data for chapter_data in media_data.get_sorted_chapters()}
+                        chapter_dirs = {self.settings.get_chapter_dir(media_data, chapter_data, skip_create=True): chapter_data for chapter_data in media_data.get_sorted_chapters()}
                         for chapter_dir in os.listdir(media_path):
                             chapter_path = os.path.join(media_path, chapter_dir)
-                            if os.path.isdir(chapter_path) and chapter_path not in chapter_dirs:
+                            if chapter_path not in chapter_dirs and os.path.isdir(chapter_path):
                                 logging.info("Removing %s because chapter info has been removed", chapter_path)
                                 shutil.rmtree(chapter_path)
 

amt/tests/test.py
@@ -1250,6 +1250,13 @@ class ArgsTest(MinimalUnitTestClass):
         for dir in os.listdir(self.settings.media_dir):
             self.assertEqual(0, len(os.listdir(os.path.join(self.settings.media_dir, dir))))
 
+    def test_clean_noop(self):
+        a = self.add_test_media(self.test_server, limit=1)
+        self.media_reader.download_unread_chapters()
+        parse_args(media_reader=self.media_reader, args=["clean"])
+        print(a)
+        self.verify_all_chapters_downloaded()
+
     def test_clean_read(self):
         self.add_test_media(self.test_server)
         self.media_reader.download_unread_chapters()