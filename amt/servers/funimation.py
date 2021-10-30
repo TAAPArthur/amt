edc748f8d7c75e68264f65ccaf2e92766333e7ff
Arthur Williams <taaparthur@gmail.com>
2021-10-29 20:20:37 -0700

    funimationlib: new anime server
    Add support for Funimation's digital copy. Now all media that can be
    accessed with the user's account on Funimation should be available.

--- amt/servers/funimation.py
+++ amt/servers/funimation.py
@@ -8,29 +8,21 @@ from ..server import Server, get_extension
 from ..util.media_type import MediaType
 
 
-class Funimation(Server):
-    id = "funimation"
+class GenericFunimation(Server):
+    alias = "funimation"
 
     CSRF_NAME = "csrfmiddlewaretoken"
     domain = "funimation.com"
     login_url = "https://www.funimation.com/log-in/"
+    prod_api_base = "https://prod-api-funimationnow.dadcdigital.com"
+    login_api_url = prod_api_base + "/api/auth/login/"
+    episode_url = prod_api_base + "/api/funimation/episodes/?limit=99999&title_id={}"
+
     api_base = "https://www.funimation.com/api"
-    login_api_url = "https://prod-api-funimationnow.dadcdigital.com/api/auth/login/"
     show_api_url = api_base + "/experience/{}/"
     sources_api_url = api_base + "/showexperience/{}/?pinst_id=23571113"
-    episode_url = "https://prod-api-funimationnow.dadcdigital.com/api/funimation/episodes/?limit=99999&title_id={}"
-
-    new_api_episdoe_url = "https://title-api.prd.funimationsvc.com/v1/shows/{}/episodes/{}/?region=US&deviceType=web&locale=en"
-
-    season_regex = re.compile(r"var titleData\s*=\s*(.*)")
-    player_regex = re.compile(r"/player/(\d*)")
-
-    search_url = "https://api-funimation.dadcdigital.com/xml/longlist/content/page/?id=search&q={}"
-    list_url = "https://api-funimation.dadcdigital.com/xml/longlist/content/page/?id=shows&limit={}"
-    # list_url = "https://prod-api-funimationnow.dadcdigital.com/api/funimation/shows/"
 
     media_type = MediaType.ANIME
-    stream_url_regex = re.compile("funimation.com/v/([^/]*)/([^/]*)")
 
     def _get_csrf(self):
         r = self.session_get(self.login_url)
@@ -38,9 +30,7 @@ class Funimation(Server):
         return soup.find("input", {"name": self.CSRF_NAME})["value"]
 
     def needs_authentication(self):
-        if self.session.cookies.get("src_user_id", domain=self.domain):
-            return False
-        return True
+        return not self.session.cookies.get("src_user_id", domain=self.domain)
 
     @property
     def is_premium(self):
@@ -62,6 +52,39 @@ class Funimation(Server):
         except KeyError:
             logging.info(data["error"])
             return False
+
+    def download_subtitles(self, media_data, chapter_data, dir_path):
+        r = self.session_get(self.show_api_url.format(chapter_data["alt_id"]))
+
+        for season in r.json()["seasons"]:
+            for chapter in season["episodes"]:
+                video = chapter["languages"][media_data["lang"].lower()]["alpha"]
+                exp = video["simulcast"] if "simulcast" in video else video["uncut"]
+                if exp["experienceId"] == int(chapter_data["alt_id"]) and "textTracks" in exp["sources"][0]:
+                    for track in exp["sources"][0]["textTracks"]:
+                        if self.settings.is_allowed_text_lang(track["language"], media_data):
+                            subtitle_src = track["src"]
+                            ext = get_extension(subtitle_src)
+                            basename = self.settings.get_page_file_name(media_data, chapter_data, ext=ext)
+                            path = os.path.join(dir_path, basename)
+                            if not os.path.exists(path):
+                                r = self.session_get(subtitle_src)
+                                with open(path, "wb") as fp:
+                                    fp.write(r.content)
+                    break
+
+    def get_stream_urls(self, media_data=None, chapter_data=None):
+        chapter_id = chapter_data["alt_id"]
+        r = self.session_get(self.sources_api_url.format(chapter_id))
+        return [item["src"] for item in r.json()["items"]]
+
+
+class Funimation(GenericFunimation):
+    id = "funimation"
+    search_url = "https://api-funimation.dadcdigital.com/xml/longlist/content/page/?id=search&q={}"
+    list_url = "https://api-funimation.dadcdigital.com/xml/longlist/content/page/?id=shows&limit={}"
+    new_api_episdoe_url = "https://title-api.prd.funimationsvc.com/v1/shows/{}/episodes/{}/?region=US&deviceType=web&locale=en"
+    stream_url_regex = re.compile("funimation.com/v/([^/]*)/([^/]*)")
 
     def _get_media_list(self, url, limit=None):
         r = self.session_get(url)
@@ -85,7 +108,7 @@ class Funimation(Server):
         return self._get_media_list(self.list_url.format(limit if limit else 0))
 
     def search(self, term, alt_id=None, limit=2):
-        return self._get_media_list(self.search_url.format(term.replace(" ", "%20")), limit=limit)
+        return self._get_media_list(self.search_url.format(term), limit=limit)
 
     def _get_episode_id(self, url):
         match = self.stream_url_regex.search(url)
@@ -128,27 +151,44 @@ class Funimation(Server):
         chapter_id = self._get_episode_id(url)
         return chapter_id
 
-    def get_stream_urls(self, media_data=None, chapter_data=None):
-        chapter_id = chapter_data["alt_id"]
-        r = self.session_get(self.sources_api_url.format(chapter_id))
-        return [item["src"] for item in r.json()["items"]]
 
-    def download_subtitles(self, media_data, chapter_data, dir_path):
-        r = self.session_get(self.show_api_url.format(chapter_data["alt_id"]))
+class FunimationLibrary(GenericFunimation):
+    id = "funimationlib"
+    has_free_chapters = False
 
-        for season in r.json()["seasons"]:
-            for chapter in season["episodes"]:
-                video = chapter["languages"][media_data["lang"]]["alpha"]
-                exp = video["simulcast"] if "simulcast" in video else video["uncut"]
-                if exp["experienceId"] == int(chapter_data["alt_id"]) and "textTracks" in exp["sources"][0]:
-                    for track in exp["sources"][0]["textTracks"]:
-                        if self.settings.is_allowed_text_lang(track["language"], media_data):
-                            subtitle_src = track["src"]
-                            ext = get_extension(subtitle_src)
-                            basename = self.settings.get_page_file_name(media_data, chapter_data, ext=ext)
-                            path = os.path.join(dir_path, basename)
-                            if not os.path.exists(path):
-                                r = self.session_get(subtitle_src)
-                                with open(path, "wb") as fp:
-                                    fp.write(r.content)
-                    break
+    list_url = GenericFunimation.prod_api_base + "/api/funimation/library/"
+    search_url = GenericFunimation.prod_api_base + "/api/funimation/library/?search={}"
+    episode_url = GenericFunimation.prod_api_base + "/api/funimation/library/?show={}&season={}"
+    languages = ["Japanese", "English"]
+
+    def _get_media_list_helper(self, data):
+        for media_info in data["items"]:
+            for season in media_info["seasons"]:
+                for lang in self.languages:
+                    yield self.create_media_data(id=media_info["slug"], name=media_info["title"], season_id=season["id"], season_title=season["title"], alt_id=media_info["id"], lang=lang, season_number=season["number"])
+
+    def get_auth_header(self):
+        t = self.session.cookies.get("src_token", domain=self.domain)
+        return {"Authorization": "Token " + t}
+
+    def get_media_list(self, limit=None):
+        if self.needs_authentication():
+            return []
+        data = self.session.get(self.list_url, headers=self.get_auth_header()).json()
+        return list(self._get_media_list_helper(data))[:limit]
+
+    def search(self, term, limit=None):
+        if self.needs_authentication():
+            return []
+        data = self.session.get(self.search_url.format(term), headers=self.get_auth_header()).json()
+        return list(self._get_media_list_helper(data))[:limit]
+
+    def update_media_data(self, media_data):
+        data = self.session.get(self.episode_url.format(media_data["alt_id"], media_data["season_number"]), headers=self.get_auth_header()).json()
+        for media_info in data["items"]:
+            for season in media_info["seasons"]:
+                if season["id"] == media_data["season_id"]:
+                    for chapter in season["episodes"]:
+                        exp = list(filter(lambda x: x["language"] == media_data["lang"], chapter["experiences"]))
+                        if exp:
+                            self.update_chapter_data(media_data, id=chapter["id"], number=chapter["number"] or 1, title=chapter["title"], premium=True, alt_id=exp[-1]["id"])
