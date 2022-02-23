5a1cae7b3e1625e40352d567befa4a943cd22e24
Arthur Williams <taaparthur@gmail.com>
2022-02-23 01:22:29 -0800

    mangadex: ignore chapters with zero pages
    Apparently mangadex has some chapters with zero pages. Don't know why
    but this causes tests to fail as they verify something was actually
    downloaded. Now we filter out chapters with zero pages which is probably
    sane behavior in general.

--- amt/servers/mangadex.py
+++ amt/servers/mangadex.py
@@ -55,7 +55,7 @@ class Mangadex(Server):
             for chapter_data in sorted(data["data"], key=lambda x: x["attributes"]["publishAt"], reverse=True):
                 attr = chapter_data["attributes"]
                 if self.settings.is_allowed_text_lang(attr["translatedLanguage"], media_data):
-                    if attr["chapter"] not in visited_chapter_numbers:
+                    if attr["pages"] and attr["chapter"] not in visited_chapter_numbers:
                         visited_chapter_numbers.add(attr["chapter"])
                         self.update_chapter_data(media_data, id=chapter_data["id"], number=attr["chapter"], title=attr["title"])
             offset += data["limit"]
